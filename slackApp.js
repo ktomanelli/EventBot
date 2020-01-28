db4e9f4937a9d1ecd44b445eb2229b0be3f0a1d0
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-01-28 16:00:13 -0500

    changed time to run from 9 to 10am

--- slackApp.js
+++ slackApp.js
@@ -95,11 +95,11 @@ async function eventData(links, int, wd) {
 setInterval(function() {
   const today = new Date(Date.now);
   if (today.getDay === 0) {
-    if (today.getHours === 9 && today.getMinutes === 0) {
+    if (today.getHours === 10 && today.getMinutes === 0) {
       eventData(sources, 7, 0);
       eventData(sources, 1, 1);
     }
-  } else if (today.getHours === 9 && today.getMinutes === 0) {
+  } else if (today.getHours === 10 && today.getMinutes === 0) {
     eventData(sources, 1, 1);
   }
 }, 60000);
