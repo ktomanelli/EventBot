4c1a035750950731842ec853e46ae88da70126a7
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-02-10 15:14:09 -0500

    overhauled the weekly message
    JP helped implement a reduce to get data in a format which would be easier to work with. now sending logo for group and then sending events as replies in a thread for each group

--- eventBot.js
+++ eventBot.js
@@ -1,19 +1,93 @@
 require('dotenv').config();
 const { RTMClient, WebClient } = require('@slack/client');
 
+let lastEventSource = '';
 const token = process.env.XOXB;
 if (!token) {
   console.log('You must specify a token to use this example');
   return;
 }
+
 const web = new WebClient(token);
 const conversationID = process.env.CHANNEL_ID;
 
+function compare(a, b) {
+  let comparison = 0;
+  if (a.source === b.source) {
+    if (new Date(a.date).getTime() > new Date(b.date).getTime()) {
+      comparison = 1;
+    } else if (a.date < b.date) {
+      comparison = -1;
+    }
+  } else if (a.source > b.source) {
+    comparison = 1;
+  } else if (a.source < b.source) {
+    comparison = -1;
+  }
+  return comparison;
+}
+async function sendWeekly(events) {
+  const postContent = {
+    channel: conversationID,
+    // thread_ts: '',
+  };
+  const postPic = {
+    channel: conversationID,
+  };
+
+  const sortedEvents = events.sort(compare);
+  if (events.length !== 0) {
+    const groupedEvents = sortedEvents.reduce((grouped, event) => {
+      if (!grouped[event.source]) {
+        grouped[event.source] = { image: '', events: [] };
+      }
+      grouped[event.source].image = event.image_url;
+      grouped[event.source].events.push(event);
+      return grouped;
+    }, {});
+    for (let i = 0; i < Object.keys(groupedEvents).length; i += 1) {
+      postContent.text = '';
+      postPic.attachments = `[{
+        'title':'${
+          groupedEvents[Object.keys(groupedEvents)[i]].events[0].source
+        }',
+        'image_url':'${
+          groupedEvents[Object.keys(groupedEvents)[i]].events[0].image_url
+        }'}]`;
+      postContent.thread_ts = await web.chat
+        .postMessage(postPic)
+        .then(res => {
+          console.log(`Message sent: `, res.ts);
+          return res.ts;
+        })
+        .catch(console.error);
+      for (
+        let x = 0;
+        x < groupedEvents[Object.keys(groupedEvents)[i]].events.length;
+        x += 1
+      ) {
+        postContent.text += `\n*${
+          groupedEvents[Object.keys(groupedEvents)[i]].events[x].title
+        }*\n${groupedEvents[Object.keys(groupedEvents)[i]].events[x].date}\n<${
+          groupedEvents[Object.keys(groupedEvents)[i]].events[x].url
+        }|Click Here for More Info!>\n`;
+      }
+      web.chat
+        .postMessage(postContent)
+        .then(res => {
+          console.log(`Message sent: `, res.ts);
+        })
+        .catch(console.error);
+    }
+  }
+}
+
 function slackSend(weeklyOrDaily, events) {
-  console.log(events);
   // weeklyOrDaily is whether sending a weekly message or a daily message
   // weekly = 0
   // daily = 1
+  let initMessage = '';
+  /** ****WEEKLY***** */
   if (weeklyOrDaily === 0) {
     const wd = 'this week.';
     if (events.length !== 0) {
@@ -24,23 +98,15 @@ function slackSend(weeklyOrDaily, events) {
         })
         .then(res => {
           console.log('Message sent: ', res.ts);
+          initMessage = res.ts;
         })
         .catch(console.error);
     }
     setTimeout(function() {
-      events.forEach(event => {
-        const slackMessage = `\n*${event.title}*\n${event.date}\n<${event.url}|Click Here for More Info!>\n `;
-        if (events.length !== 0) {
-          web.chat
-            .postMessage({ channel: conversationID, text: `${slackMessage}\n` })
-            .then(res => {
-              console.log('Message sent: ', res.ts);
-            })
-            .catch(console.error);
-        }
-      });
-    }, 1000);
+      sendWeekly(events);
+    }, 500);
   } else {
+    /** ***DAILY**** */
     const wd = 'today.\n';
     let slackMessage = `There are ${events.length} events ${wd} \n`;
     if (events.length !== 0) {
@@ -56,7 +122,7 @@ function slackSend(weeklyOrDaily, events) {
     }
     setTimeout(function() {
       events.forEach(event => {
-        slackMessage = `\n*${event.title}*\n${event.date}\n<${event.url}|Click Here for More Info!>\n `;
+        slackMessage = `\n*${event.title}*\n${event.date}\n<${event.url}|Click Here for More Info!>\n`;
         if (events.length !== 0) {
           web.chat
             .postMessage({ channel: conversationID, text: `${slackMessage}\n` })
@@ -65,6 +131,7 @@ function slackSend(weeklyOrDaily, events) {
             })
             .catch(console.error);
         }
+        lastEventSource = event.source;
       });
     }, 1000);
   }
--- slackApp.js
+++ slackApp.js
@@ -8,23 +8,38 @@ const oneWeekFromNowEpoch = new Date();
 oneWeekFromNowEpoch.setDate(oneWeekFromNowEpoch.getDate() + 7);
 const oneWeekFromNow = oneWeekFromNowEpoch.toISOString();
 
-const astoriaCreatives = 'http://api.meetup.com/AstoriaCreatives/events';
-const astoriaWriters = 'http://api.meetup.com/astoriawritersgroup/events';
-const astoriaTech = 'http://api.meetup.com/Astoria-Tech-Meetup/events';
-const queensTech = 'http://api.meetup.com/Queens-Tech-Night/events';
-const designDriven = 'http://api.meetup.com/Design-Driven-NYC/events/';
-const hardWired = 'https://www.meetup.com/Hardwired-NYC/events/';
-const qed = `https://www.googleapis.com/calendar/v3/calendars/b9cmt5r1ikc6ho8ego41l9dl5iijj2nf@import.calendar.google.com/events?key=${process.env.GOOGLE_KEY}&timeMin=${timeNow}&timeMax=${oneWeekFromNow}`;
 const typeThursdayNYC = '';
 const bookCulture = 'https://www.bookculture.com/event';
-const sources = [
-  astoriaCreatives,
-  astoriaWriters,
-  astoriaTech,
-  queensTech,
-  designDriven,
-  qed,
-];
+const sources = {
+  AstoriaCreatives: [
+    'http://api.meetup.com/AstoriaCreatives/events',
+    'https://i.ibb.co/NVZRr3k/Astoria-Creatives.jpg',
+  ],
+  AstoriaWriters: [
+    'http://api.meetup.com/astoriawritersgroup/events',
+    'https://i.ibb.co/M8FrVh5/astoria-Writers.png',
+  ],
+  AstoriaTech: [
+    'http://api.meetup.com/Astoria-Tech-Meetup/events',
+    'https://i.ibb.co/PtkWqxt/astoria-Tech.jpg',
+  ],
+  QueensTech: [
+    'http://api.meetup.com/Queens-Tech-Night/events',
+    'https://i.ibb.co/x6jPrK5/Queens-Tech.png',
+  ],
+  DesignDriven: [
+    'http://api.meetup.com/Design-Driven-NYC/events/',
+    'https://i.ibb.co/jhBZtZB/Design-Driven.png',
+  ],
+  HardWired: [
+    'https://api.meetup.com/Hardwired-NYC/events/',
+    'https://i.ibb.co/5B7M8x3/Hard-Wired.png',
+  ],
+  QED: [
+    `https://www.googleapis.com/calendar/v3/calendars/b9cmt5r1ikc6ho8ego41l9dl5iijj2nf@import.calendar.google.com/events?key=${process.env.GOOGLE_KEY}&timeMin=${timeNow}&timeMax=${oneWeekFromNow}`,
+    'https://i.ibb.co/ctqyZWm/QED.png',
+  ],
+};
 
 function fixDate(dataDate) {
   const options = {
@@ -41,19 +56,26 @@ function fixDate(dataDate) {
   }
   const dateObj = new Date(dateStr);
   return dateObj.toLocaleDateString('en-US', options);
-
-  // unfinished, this would re arange date so it's easier to read
-  // dataDate.sort(function(a, b) {
-  //   return a - b;
-  // });
+}
+function getKeyByValue(obj, val) {
+  return Object.keys(obj).find(key => obj[key][0] === val);
 }
 async function getEvents(links, timeFrame) {
   try {
+    const urls = Object.values(links);
+    const eventLinks = [];
+    const imgLinks = [];
+    for (let i = 0; i < urls.length; i += 1) {
+      eventLinks.push(urls[i][0]);
+      imgLinks.push(urls[i][1]);
+    }
     const events = [];
     let response = '';
-    for (let x = 0; x < links.length; x += 1) {
-      response = await axios.get(links[x]);
-      if (links[x] === qed) {
+    for (let x = 0; x < eventLinks.length; x += 1) {
+      const sourceCheck = getKeyByValue(links, eventLinks[x]);
+
+      response = await axios.get(eventLinks[x]);
+      if (eventLinks[x] === sources.QED[0]) {
         for (let i = 0; i < response.data.items.length; i += 1) {
           if (
             Date.parse(response.data.items[i].start.dateTime) <
@@ -61,10 +83,11 @@ async function getEvents(links, timeFrame) {
             Date.parse(response.data.items[i].start.dateTime) > Date.now()
           ) {
             events.push({
-              source: links[x],
+              source: sourceCheck,
               title: response.data.items[i].summary,
               date: fixDate(response.data.items[i].start.dateTime),
               url: response.data.items[i].htmlLink,
+              image_url: `${links[sourceCheck][1]}`,
             });
           }
         }
@@ -75,10 +98,11 @@ async function getEvents(links, timeFrame) {
             response.data[i].time > Date.now()
           ) {
             events.unshift({
-              source: links[x],
+              source: sourceCheck,
               title: response.data[i].name,
               date: fixDate(response.data[i].time),
               url: response.data[i].link,
+              image_url: `${links[sourceCheck][1]}`,
             });
           }
         }
@@ -99,16 +123,18 @@ async function eventData(links, int, wd) {
   }
 }
 
-eventData(sources, 1, 1);
+// eventData(sources, 1, 1);
 
-// setInterval(function() {
-//   const today = new Date();
-//   if (today.getDay() === 0) {
-//     if (today.getHours() === 10 && today.getMinutes() === 0) {
-//       eventData(sources, 7, 0);
-//       eventData(sources, 1, 1);
-//     }
-//   } else if (today.getHours() === 10 && today.getMinutes() === 1) {
-//     eventData(sources, 1, 1);
-//   }
-// }, 60000);
+setInterval(function() {
+  const today = new Date();
+  if (today.getDay() === 0) {
+    if (today.getHours() === 10 && today.getMinutes() === 0) {
+      eventData(sources, 7, 0);
+      if (today.getHours() === 10 && today.getMinutes() === 1) {
+        eventData(sources, 1, 1);
+      }
+    }
+  } else if (today.getHours() === 10 && today.getMinutes() === 0) {
+    eventData(sources, 1, 1);
+  }
+}, 60000);
